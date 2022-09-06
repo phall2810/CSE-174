be53e3eb2031cea2be90d4964dd19ec2e47c3f98
phall2810 <112958510+phall2810@users.noreply.github.com>
2022-09-06 10:48:44 -0400

    Update WordleWizard.java

--- WordleWizard.java
+++ WordleWizard.java
@@ -2,6 +2,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// Description: You will build a program that will ask the user to supply a list of words via a text file
+// (they will provide the file name), and from that list, the program will print out only those words
+// that are five letters long, to help narrow down to only valid Wordle words. You will have no idea
+// how many words are in the file.
+
 public class WordleWizard {
 
   public static void main(String[] args) throws FileNotFoundException {
