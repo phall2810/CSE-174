0ec0ef35483e4123903138e595a678a72f82fb2b
phall2810 <112958510+phall2810@users.noreply.github.com>
2022-09-06 10:53:55 -0400

    Add files via upload

---
+++ SunshineArray.java
@@ -0,0 +1,42 @@
+import java.util.Scanner; //Needed for user input
+
+public class SunshineArray {
+   
+  public static void main(String[] args) {
+    int rows;
+    int cols; 
+    int value;
+    Scanner keyboardReader = new Scanner(System.in);
+    System.out.print("How many rows for your table? ");
+    rows = keyboardReader.nextInt();
+    System.out.print("How many cols for your table? ");
+    cols = keyboardReader.nextInt();
+    System.out.print("What value should fill your table? ");
+    value = keyboardReader.nextInt();
+    int[] array = makeArray(cols, value);
+    printArray(array, rows);
+  }  
+   
+  //method that takes the size of the array as well as the default value
+  public static int[] makeArray(int cols, int value) {
+    int[] array = new int[cols];
+    for (int i = 0; i < cols; i++) {
+      array[i] = value;
+    } 
+    return array;
+  }
+   
+  public static void printArray(int[] array, int rows) {
+    System.out.println();
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < array.length; j++) {
+        System.out.print(array[j] + " ");
+     
+       
+      }
+      System.out.println();
+    } 
+
+  }
+}
+
